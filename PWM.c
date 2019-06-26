871bb11d53af320bcd43057756407a1411fb0688
kulkarnipurvesh <38707882+kulkarnipurvesh@users.noreply.github.com>
2019-06-26 15:02:41 -0700

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -20,8 +20,8 @@ duty cycle on GPIO2 and GPIO3 (phase shifted at
 #define GPIO_PF3_M1PWM7         0x00050C05
 
 int main()
-{
-	SysCtlClockSet(SYSCTL_SYSDIV_32|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+{	
+    SysCtlClockSet(SYSCTL_SYSDIV_32|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
 
     // Enable the peripherals used by this program.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
@@ -29,7 +29,7 @@ int main()
 
     //Configure PF1 Pins as PWM
     GPIOPinConfigure(GPIO_PF2_M1PWM6);
-	GPIOPinConfigure(GPIO_PF3_M1PWM7);
+    GPIOPinConfigure(GPIO_PF3_M1PWM7);
     GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2|GPIO_PIN_3);
 
     //Configure PWM Options
@@ -42,17 +42,15 @@ int main()
 
     //Set PWM duty-50% (Period /2)
     PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,813); 
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,813); 
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,813); 
 
     // Enable the PWM generator
     PWMGenEnable(PWM1_BASE, PWM_GEN_3);
 
     // Turn on the Output pins
     PWMOutputState(PWM1_BASE, PWM_OUT_6_BIT, true);  // output a wave on pin (PWM1_BASE = MODULE 1, PWM_OUT_6 = PF2 (CHECK SCOPE))
-	PWMOutputInvert(PWM1_BASE, PWM_OUT_7_BIT, true); // Invert 
-	PWMOutputState(PWM1_BASE, PWM_OUT_7_BIT, true);  // Enable Interted Output
+    PWMOutputInvert(PWM1_BASE, PWM_OUT_7_BIT, true); // Invert 
+    PWMOutputState(PWM1_BASE, PWM_OUT_7_BIT, true);  // Enable Interted Output
 		
-
-	while(1) {}
-        
-	}
+    while(1) {}    
+}
